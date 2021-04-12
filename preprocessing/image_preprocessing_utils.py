8a5c7d7e5c47bae672c2cc599f0808b205554254
Dresvyanskiy <denis.dresvyanskiy@uni-ulm.de>
2021-04-12 15:55:13 +0200

    Module for image preprocessing (loading, saving, etc.) was created.

---
+++ preprocessing/image_preprocessing_utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+TODO: write description of module
+"""
+
+from PIL import Image
+import numpy as np
+import pandas as pd
+
+__author__ = "Denis Dresvyanskiy"
+__copyright__ = "Copyright 2021"
+__credits__ = ["Denis Dresvyanskiy"]
+__maintainer__ = "Denis Dresvyanskiy"
+__email__ = "denis.dresvyanskiy@uni-ulm.de"
+
+
+def load_image(path:str)-> np.ndarray:
+    # TODO: write description
+    with Image.open(path) as im:
+        return np.ndarray(im)
+
+def save_image(img:np.ndarray, path_to_output:str)->None:
+    # TODO: write description
+    img = Image.fromarray(img)
+    img.save(path_to_output)
+
