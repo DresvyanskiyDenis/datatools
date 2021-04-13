a18d4f310bb9e7ecb8b0dfa5012a56ea0b64388c
Denis <ecodenis@yandex.ru>
2021-04-13 21:00:38 +0200

    The extracting frames from all videos function was changes so as now it walk through all the directories and subdirectories of defined directory.

--- preprocessing/video_preprocessing_utils.py
+++ preprocessing/video_preprocessing_utils.py
@@ -21,8 +21,9 @@ def extract_frames_from_videofile(input_path:str, output_path:str)->None:
     # check if videofile was opened:
     if videofile is None or not videofile.isOpened():
         raise Exception('Warning: unable to open video source: ', input_path)
-    #calculate the name of videofile
+    #calculate the name of videofile and its extention
     video_filename=re.split(r'\\|/',input_path)[-1].split('.')[0]
+    file_extention=re.split(r'\\|/',input_path)[-1].split('.')[-1]
     video_frame_rate=videofile.get(cv2.CAP_PROP_FPS)
     # creating directories for saving files
     if not os.path.exists(os.path.join(output_path,video_filename,video_filename+'_frames')):
@@ -46,7 +47,7 @@ def extract_frames_from_videofile(input_path:str, output_path:str)->None:
     with open(os.path.join(output_path,video_filename, "%s_%s.txt"%(video_filename, 'metadata'))
             , "w") as metadata:
         metadata.write('filename:%s\n'
-                       'frame_rate:%f\n'%(video_filename, video_frame_rate))
+                       'frame_rate:%f\n'%(video_filename+"."+file_extention, video_frame_rate))
     # Release all space and windows once done
     videofile.release()
     cv2.destroyAllWindows()
@@ -54,18 +55,24 @@ def extract_frames_from_videofile(input_path:str, output_path:str)->None:
 
 def extract_frames_from_all_videos_in_dir(input_dir:str, output_dir:str)-> None:
     # TODO: add description
-    list_video_filenames=os.listdir(input_dir)
+    absolute_paths_to_videos=[]
+    # walk through all the subdirectories and find all the videofiles and their absolute paths
+    for root, dirs, files in os.walk(input_dir):
+        for file in files:
+            if file.endswith((".avi",".mp4")):
+                absolute_paths_to_videos.append(os.path.join(root, file))
     # create output directory
     if not os.path.exists(output_dir):
         os.makedirs(output_dir, exist_ok=True)
     # extract frames file-by-file
-    for video_filename in list_video_filenames:
-        extract_frames_from_videofile(input_path=os.path.join(input_dir,video_filename),
-                                      output_path=os.path.join(output_dir,video_filename))
+    for absolute_path_to_video in absolute_paths_to_videos:
+        video_filename=re.split(r'\\|/',absolute_path_to_video)[-1].split('.')[0]
+        extract_frames_from_videofile(input_path=absolute_path_to_video,
+                                      output_path=output_dir)
 
 
 
 if __name__ == '__main__':
-    input_path=r'D:\Databases\DAiSEE\DAiSEE\DataSet\Train\181374\181374015\181374015.avi'
-    output_path=r'C:\tmp'
-    extract_frames_from_videofile(input_path, output_path)
+    input_path=r'E:\Databases\DAiSEE\DAiSEE\DataSet\Train'
+    output_path=r'E:\Databases\DAiSEE\frames'
+    extract_frames_from_all_videos_in_dir(input_path, output_path)
