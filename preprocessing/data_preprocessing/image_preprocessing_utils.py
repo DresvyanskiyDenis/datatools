d037da464202d8ebdb0e88e931c1cdc7d07e1cad
Denis <ecodenis@yandex.ru>
2021-04-18 18:31:32 +0200

    All preprocessing image functions are now tested and corrected. It is stable version now.

--- preprocessing/data_preprocessing/image_preprocessing_utils.py
+++ preprocessing/data_preprocessing/image_preprocessing_utils.py
@@ -22,7 +22,7 @@ __email__ = "denis.dresvyanskiy@uni-ulm.de"
 def load_image(path:str)-> np.ndarray:
     # TODO: write description
     with Image.open(path) as im:
-        return np.ndarray(im)
+        return np.array(im)
 
 def save_image(img:np.ndarray, path_to_output:str)->None:
     # TODO: write description
@@ -35,6 +35,9 @@ def resize_image(img:np.ndarray, new_size:Tuple[int, int])-> np.ndarray:
     img=img.resize(new_size)
     return np.array(img)
 
+def show_image(img:np.ndarray)->None:
+    Image.fromarray(img).show()
+
 
 def shear_image(img:np.ndarray, shear_factor:float)->np.ndarray:
     # TODO: write description
@@ -42,12 +45,12 @@ def shear_image(img:np.ndarray, shear_factor:float)->np.ndarray:
     afine_tf = transform.AffineTransform(shear=shear_factor)
     # Apply transform to image data
     modified_img = transform.warp(img, inverse_map=afine_tf)
-    return modified_img
+    return (modified_img*255.).astype('uint8')
 
 def rotate_image(img:np.ndarray, rotation_angle:int)->np.ndarray:
     # TODO: write description
-    modified_image=transform.rotate(img, rotation_angle)
-    return modified_image
+    modified_image=transform.rotate(img, rotation_angle)*255.
+    return modified_image.astype('uint8')
 
 def flip_image(img:np.ndarray, flip_type:str)->np.ndarray:
     # TODO: write description
@@ -59,9 +62,9 @@ def flip_image(img:np.ndarray, flip_type:str)->np.ndarray:
         modified_image=img[::-1,:]
     return modified_image
 
-def shift_image(img:np.ndarray, shift_factor:float)->np.ndarray:
+def shift_image(img:np.ndarray, shift_vector:Tuple[float, float])->np.ndarray:
     # TODO: write description
-    transformation = transform.AffineTransform(translation=img)
+    transformation = transform.AffineTransform(translation=shift_vector)
     shifted = transform.warp(img, transformation, mode='wrap', preserve_range=True)
     shifted = shifted.astype(img.dtype)
     return shifted
@@ -73,20 +76,20 @@ def change_brightness(img:np.ndarray, brightness_factor:float)->np.ndarray:
 
 def zoom_image(img:np.ndarray, zoom_factor:float)->np.ndarray:
     # TODO: write description
-    result_image=ndimage.zoom(img, zoom_factor)
+    result_image=ndimage.zoom(img, (zoom_factor, zoom_factor, 1))
     return result_image
 
 def channel_random_noise(img:np.ndarray, num_channel:int, std:float)->np.ndarray:
     # TODO: write description
     image_size=img.shape[:2]
     noise=np.random.normal(scale=std,size=image_size)
-    modified_image=img.copy()
+    modified_image=img.copy().astype('float32')
     modified_image[:,:,num_channel]+=noise
-    return modified_image
+    return modified_image.astype('uint8')
 
 def crop_image(img:np.ndarray, bbox:Tuple[int,int,int,int])->np.ndarray:
     # TODO: write description
-    x0,x1,y0,y1 = bbox
+    x0,y0,x1,y1 = bbox
     if x0<0 or x1>img.shape[1] or y0<0 or y1>img.shape[0]:
         raise AttributeError("Some coordinates of bbox are negative or greater than "
                              "the image size. Provided bbox:%s"%bbox)
@@ -94,16 +97,23 @@ def crop_image(img:np.ndarray, bbox:Tuple[int,int,int,int])->np.ndarray:
 
 def blur_image(img:np.ndarray, sigma:float=3)->np.ndarray:
     # TODO: write description
-    modified_image=ndimage.gaussian_filter(img, sigma=sigma)
+    modified_image=ndimage.gaussian_filter(img, sigma=(sigma, sigma, 0))
     return modified_image
 
 def get_image_with_worse_quality(img:np.ndarray, rescaling_factor:float)->np.ndarray:
     # TODO: write description
-    modified_image=transform.rescale(img, rescaling_factor, anti_aliasing=False)
+    modified_image=transform.rescale(img, (rescaling_factor, rescaling_factor, 1), anti_aliasing=False)
     modified_image=transform.resize(modified_image, img.shape[:2])
-    return modified_image
+    return (modified_image*255).astype('uint8')
 
 def scale_img_to_0_1(img:np.ndarray)->np.ndarray:
     # TODO: write description
     modified_image=img/255.
     return modified_image
+
+
+if __name__ == "__main__":
+    img=load_image(r'E:\Databases\DAiSEE\DAiSEE\train_preprocessed\sorted_faces\1\1100142033_10.jpg')
+    show_image(img)
+    img=shear_image(img, -0.5)
+    show_image(img)
