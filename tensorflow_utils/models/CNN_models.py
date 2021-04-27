3e14ee77709108063a4d529351bbeb5d8f2cf099
Denis <ecodenis@yandex.ru>
2021-04-27 23:21:56 +0200

    New pretrained on emotion recognition task model was added.

--- tensorflow_utils/models/CNN_models.py
+++ tensorflow_utils/models/CNN_models.py
@@ -93,11 +93,71 @@ def get_modified_VGGFace2_resnet_model(dense_neurons_after_conv: Tuple[int,...],
     return model
 
 
+def get_EMO_VGGFace2(path:str)->tf.keras.Model:
+    # construct old model
+    pretrained_VGGFace2 = _get_pretrained_VGGFace2_model("", pretrained=False)
+    x = pretrained_VGGFace2.get_layer('activation_48').output
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Dense(units=512, activation='relu')(x)
+    x = tf.keras.layers.Dropout(0.5)(x)
+    x = tf.keras.layers.Dense(units=7, activation='softmax')(x)
+    EMO_VGGFace2_model = tf.keras.Model(inputs=pretrained_VGGFace2.inputs, outputs=[x])
+    EMO_VGGFace2_model.load_weights(path)
+    del pretrained_VGGFace2
+    tf.keras.backend.clear_session()
+    return EMO_VGGFace2_model
+
+def get_pretrained_modified_EMO_VGGFace2(dense_neurons_after_conv: Tuple[int,...],
+                                       dropout: float = 0.3,
+                                       regularization:Optional[tf.keras.regularizers.Regularizer]=None,
+                                       output_neurons: Union[Tuple[int,...], int] = 7, pooling_at_the_end: Optional[str] = None,
+                                       path_to_weights: Optional[str] = None) -> tf.keras.Model:
+    # construct old model
+    EMO_VGGFace2_model=get_EMO_VGGFace2(path_to_weights)
+    # start to construct new model by cutting off old one
+    x = EMO_VGGFace2_model.get_layer('activation_48').output
+    # take pooling or not
+    if pooling_at_the_end is not None:
+        if pooling_at_the_end == 'avg':
+            x = tf.keras.layers.GlobalAveragePooling2D()(x)
+        elif pooling_at_the_end == 'max':
+            x = tf.keras.layers.GlobalMaxPooling2D()(x)
+        else:
+            raise AttributeError(
+                'Parameter pooling_at_the_end can be either \'avg\' or \'max\'. Got %s.' % (pooling_at_the_end))
+    # create Dense layers
+    for dense_layer_idx in range(len(dense_neurons_after_conv) - 1):
+        num_neurons_on_layer = dense_neurons_after_conv[dense_layer_idx]
+        x = tf.keras.layers.Dense(num_neurons_on_layer, activation='relu', kernel_regularizer=regularization)(x)
+        if dropout:
+            x = tf.keras.layers.Dropout(dropout)(x)
+    # pre-last Dense layer
+    num_neurons_on_layer = dense_neurons_after_conv[-1]
+    x = tf.keras.layers.Dense(num_neurons_on_layer, activation='relu')(x)
+    # If outputs should be several, then create several layers, otherwise one
+    if isinstance(output_neurons, tuple):
+        output_layers = []
+        for num_output_neurons in output_neurons:
+            output_layer_i = tf.keras.layers.Dense(num_output_neurons, activation='softmax')(x)
+            # output_layer_i=tf.keras.layers.Reshape((-1, 1))(output_layer_i)
+            output_layers.append(output_layer_i)
+    else:
+        output_layers = tf.keras.layers.Dense(output_neurons, activation='softmax')(x)
+        # in tf.keras.Model it should be always a list (even when it has only 1 element)
+        output_layers = [output_layers]
+    # create model
+    model = tf.keras.Model(inputs=EMO_VGGFace2_model.inputs, outputs=output_layers)
+    del EMO_VGGFace2_model
+    tf.keras.backend.clear_session()
+    return model
+
+
 
 if __name__ == "__main__":
-    path_to_weights = r'D:\Downloads\vggface2_Keras\vggface2_Keras\model\resnet50_softmax_dim512\weights.h5'
-    model = get_modified_VGGFace2_resnet_model(dense_neurons_after_conv=(1024, 512),
+    path_to_weights = r'C:\Users\Denis\Downloads\Telegram Desktop\weights_0_66_37_affectnet_cat.h5'
+    #model=get_EMO_VGGFace2(path_to_weights)
+    model = get_pretrained_modified_EMO_VGGFace2(dense_neurons_after_conv=(1024, 512),
                                        dropout= 0.3, regularization=tf.keras.regularizers.l1_l2(0.0001),
                                        output_neurons = 7, pooling_at_the_end= 'avg',
-                                       pretrained= True, path_to_weights=path_to_weights)
+                                       path_to_weights=path_to_weights)
     model.summary()
