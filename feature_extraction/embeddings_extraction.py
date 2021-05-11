3aa83464df628630955cf22c997e775e532671df
Denis <ecodenis@yandex.ru>
2021-05-11 23:22:19 +0200

    A new module for extracting deep embeddings by deep neural models was created.

---
+++ feature_extraction/embeddings_extraction.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+TODO: add description
+"""
+
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+def extract_deep_embeddings_from_batch_of_images(images:np.ndarray, extractor:tf.keras.Model)->np.ndarray:
+    pass
