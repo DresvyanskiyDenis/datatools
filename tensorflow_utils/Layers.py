a2330f359644d78ebd896542e6475248599097ca
Denis <ecodenis@yandex.ru>
2021-05-13 23:07:13 +0200

    class Self_attention_pixel_wise was a little bit corrected. Now it works as it was thought (downsize_factor really downsizes the number of filters at the end of layer).

--- tensorflow_utils/Layers.py
+++ tensorflow_utils/Layers.py
@@ -32,11 +32,16 @@ class Self_attention_pixel_wise(tf.keras.layers.Layer):
         self.query_conv.build(input_shape)
         self.value_conv = tf.keras.layers.Conv2D(input_shape[-1] // self.downsize_factor, kernel_size=1, padding='same')
         self.value_conv.build(input_shape)
-        self.output_conv=tf.keras.layers.Conv2D(input_shape[-1], kernel_size=1, padding='same')
+        self.output_conv=tf.keras.layers.Conv2D(input_shape[-1]// self.downsize_factor, kernel_size=1, padding='same')
         self.output_conv.build((input_shape[0],input_shape[1],input_shape[2],input_shape[-1]//self.downsize_factor))
         # set trainable weights of this layer to be weights of all inner 1D convolutional layers
         self._trainable_weights=self.key_conv.trainable_weights+self.query_conv.trainable_weights+\
                                 self.value_conv.trainable_weights+self.output_conv.trainable_weights
+        # reduce shape of shortcut
+        if self.downsize_factor>1:
+            self.reduce_shortcut_conv=tf.keras.layers.Conv2D(input_shape[-1]//self.downsize_factor, kernel_size=1, padding='same')
+            self.reduce_shortcut_conv.build(input_shape)
+            self._trainable_weights+=self.reduce_shortcut_conv.trainable_weights
         # invoke the super.build() function as defined by keras authors
         super(Self_attention_pixel_wise, self).build(input_shape)
         self.built=True
@@ -65,14 +70,140 @@ class Self_attention_pixel_wise(tf.keras.layers.Layer):
                                                 tf.keras.backend.int_shape(output_value)[-1]))(output_value)
         output_value=self.output_conv(output_value)
         # add input to the output value (shortcut connection)
-        output = tf.keras.layers.Add()([output_value, input])
+        if self.downsize_factor>1:
+            shortcut=self.reduce_shortcut_conv(input)
+            output=tf.keras.layers.Add()([output_value, shortcut])
+        else:
+            output = tf.keras.layers.Add()([output_value, input])
         return output
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0], input_shape[1], input_shape[2],input_shape[3]//self.downsize_factor)
 
 
-#class Multi_head_self_attention_pixel_wise(tf.keras.layers.Layer):
+class _Self_attention_pixel_wise_without_shortcut(tf.keras.layers.Layer):
+    """Represents pixel-wise convolutional self-attention layer, which is also called by authors non-local block
+    https://arxiv.org/pdf/1711.07971.pdf
+    It uses inheritance from tf.keras.layers.Layer.
+    """
+    def __init__(self, downsize_factor:int=1, **kwargs):
+        """Initializes layer. Downsize_factor is needed to reduce the output number of channels
+           by defined factor (by integer division on it)
+
+        :param downsize_factor: int
+                    see __init__ description
+        :param kwargs:
+        """
+        self.downsize_factor=downsize_factor
+        super(Self_attention_pixel_wise, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        # create 1x1 convolutions for query, key, value and output
+        self.key_conv=tf.keras.layers.Conv2D(input_shape[-1]//self.downsize_factor, kernel_size=1, padding='same')
+        self.key_conv.build(input_shape)
+        self.query_conv = tf.keras.layers.Conv2D(input_shape[-1] // self.downsize_factor, kernel_size=1, padding='same')
+        self.query_conv.build(input_shape)
+        self.value_conv = tf.keras.layers.Conv2D(input_shape[-1] // self.downsize_factor, kernel_size=1, padding='same')
+        self.value_conv.build(input_shape)
+        # set trainable weights of this layer to be weights of all inner 1D convolutional layers
+        self._trainable_weights=self.key_conv.trainable_weights+self.query_conv.trainable_weights+\
+                                self.value_conv.trainable_weights
+        # invoke the super.build() function as defined by keras authors
+        super(Self_attention_pixel_wise, self).build(input_shape)
+        self.built=True
+
+    def call(self, input):
+        # extract shapes
+        batch_size, height, width, channels=input.shape
+        # key flow
+        x = self.key_conv(input)
+        output_key_conv = tf.keras.layers.Reshape((-1, tf.keras.backend.int_shape(x)[-1]))(x)
+        # query flow
+        x = self.query_conv(input)
+        output_query_conv = tf.keras.layers.Reshape(( -1, tf.keras.backend.int_shape(x)[-1]))(x)
+        output_query_conv_transpose=tf.transpose(output_query_conv, perm=[0,2,1])
+        # value flow
+        x = self.value_conv(input)
+        output_value_conv = tf.keras.layers.Reshape((-1, tf.keras.backend.int_shape(x)[-1]))(x)
+        # matrix multiplication for query and key
+        multiplicated_key_query=tf.keras.layers.Dot(axes=(2,1))([output_key_conv, output_query_conv_transpose])
+        # softmax for obtained matrix
+        softmax_output=tf.keras.layers.Softmax()(multiplicated_key_query)
+        # multiply value by obtained softmax matrix
+        output_value=tf.keras.layers.Dot(axes=(2,1))([softmax_output, output_value_conv])
+        # reshape
+        output = tf.keras.layers.Reshape((height, width,
+                                        tf.keras.backend.int_shape(output_value)[-1]//self.downsize_factor))(output_value)
+        return output
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], input_shape[1], input_shape[2],input_shape[3]//self.downsize_factor)
+
+
+class Multi_head_self_attention_pixel_wise(tf.keras.layers.Layer):
+
+    def __init__(self, num_heads:int, output_shape:int, downsize_factor:int=8,  **kwargs):
+        """Initializes layer. Downsize_factor is needed to reduce the output number of channels
+           by defined factor (by integer division on it)
+
+        :param downsize_factor: int
+                    see __init__ description
+        :param kwargs:
+        """
+        self.num_heads=num_heads
+        self.downsize_factor=downsize_factor
+        self.heads=[]
+        super(Self_attention_pixel_wise, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        for head_idx in range(len(self.heads)):
+            head=Self_attention_pixel_wise(self.downsize_factor)
+            head.build(input_shape)
+            self.heads.append(head)
+            self._trainable_weights=self._trainable_weights+head.trainable_weights
+        # invoke the super.build() function as defined by keras authors
+        super(Self_attention_pixel_wise, self).build(input_shape)
+        self.built=True
+
+    def call(self, input):
+        # extract shapes
+        batch_size, height, width, channels=input.shape
+        # go through all heads
+        head_outputs=[]
+        for head_idx in range(len(self.heads)):
+            head_output=self.heads[head_idx](input)
+            head_outputs.append(head_output)
+        # concatenate them
+        concat_layer=tf.keras.layers.concatenate(head_outputs, axis=1)
+
+
+
+        # key flow
+        x = self.key_conv(input)
+        output_key_conv = tf.keras.layers.Reshape((-1, tf.keras.backend.int_shape(x)[-1]))(x)
+        # query flow
+        x = self.query_conv(input)
+        output_query_conv = tf.keras.layers.Reshape(( -1, tf.keras.backend.int_shape(x)[-1]))(x)
+        output_query_conv_transpose=tf.transpose(output_query_conv, perm=[0,2,1])
+        # value flow
+        x = self.value_conv(input)
+        output_value_conv = tf.keras.layers.Reshape((-1, tf.keras.backend.int_shape(x)[-1]))(x)
+        # matrix multiplication for query and key
+        multiplicated_key_query=tf.keras.layers.Dot(axes=(2,1))([output_key_conv, output_query_conv_transpose])
+        # softmax for obtained matrix
+        softmax_output=tf.keras.layers.Softmax()(multiplicated_key_query)
+        # multiply value by obtained softmax matrix
+        output_value=tf.keras.layers.Dot(axes=(2,1))([softmax_output, output_value_conv])
+        # reshape and apply output conv
+        output_value = tf.keras.layers.Reshape(( height, width,
+                                                tf.keras.backend.int_shape(output_value)[-1]))(output_value)
+        output_value=self.output_conv(output_value)
+        # add input to the output value (shortcut connection)
+        output = tf.keras.layers.Add()([output_value, input])
+        return output
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], input_shape[1], input_shape[2],input_shape[3]//self.downsize_factor)
 
 
 
@@ -81,8 +212,8 @@ if __name__=="__main__":
     input_shape=(50,50,128)
     model=tf.keras.Sequential()
     model.add(tf.keras.layers.Input(input_shape))
-    model.add(Self_attention_pixel_wise())
-    model.add(Self_attention_pixel_wise())
+    model.add(Self_attention_pixel_wise(downsize_factor=2))
+    model.add(Self_attention_pixel_wise(downsize_factor=2))
     model.add(tf.keras.layers.Flatten())
     model.add(tf.keras.layers.Reshape((-1,1)))
     model.add(tf.keras.layers.GlobalAveragePooling1D())
