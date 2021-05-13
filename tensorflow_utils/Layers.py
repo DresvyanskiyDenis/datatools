3380855317bf064f3e06647790e401f3949ecfc0
Denis <ecodenis@yandex.ru>
2021-05-13 21:46:58 +0200

    Description of attention layer was added.

--- tensorflow_utils/Layers.py
+++ tensorflow_utils/Layers.py
@@ -1,10 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+TODO: add description
+"""
+
 import tensorflow as tf
 import numpy as np
 
 
 class Self_attention_pixel_wise(tf.keras.layers.Layer):
-
+    """Represents pixel-wise convolutional self-attention layer, which is also called by authors non-local block
+    https://arxiv.org/pdf/1711.07971.pdf
+    It uses inheritance from tf.keras.layers.Layer.
+    """
     def __init__(self, downsize_factor:int=1, **kwargs):
+        """Initializes layer. Downsize_factor is needed to reduce the output number of channels
+           by defined factor (by integer division on it)
+
+        :param downsize_factor: int
+                    see __init__ description
+        :param kwargs:
+        """
         self.downsize_factor=downsize_factor
         super(Self_attention_pixel_wise, self).__init__(**kwargs)
 
@@ -17,22 +33,25 @@ class Self_attention_pixel_wise(tf.keras.layers.Layer):
         self.value_conv = tf.keras.layers.Conv2D(input_shape[-1] // self.downsize_factor, kernel_size=1, padding='same')
         self.value_conv.build(input_shape)
         self.output_conv=tf.keras.layers.Conv2D(input_shape[-1], kernel_size=1, padding='same')
-        self.output_conv.build((input_shape[0],input_shape[1],input_shape[2],input_shape[-1]//4))
+        self.output_conv.build((input_shape[0],input_shape[1],input_shape[2],input_shape[-1]//self.downsize_factor))
+        # set trainable weights of this layer to be weights of all inner 1D convolutional layers
         self._trainable_weights=self.key_conv.trainable_weights+self.query_conv.trainable_weights+\
                                 self.value_conv.trainable_weights+self.output_conv.trainable_weights
+        # invoke the super.build() function as defined by keras authors
         super(Self_attention_pixel_wise, self).build(input_shape)
         self.built=True
 
     def call(self, input):
+        # extract shapes
         batch_size, height, width, channels=input.shape
-        # calculations for key
+        # key flow
         x = self.key_conv(input)
         output_key_conv = tf.keras.layers.Reshape((-1, tf.keras.backend.int_shape(x)[-1]))(x)
-        # calculations for query
+        # query flow
         x = self.query_conv(input)
         output_query_conv = tf.keras.layers.Reshape(( -1, tf.keras.backend.int_shape(x)[-1]))(x)
         output_query_conv_transpose=tf.transpose(output_query_conv, perm=[0,2,1])
-        # calculations for value
+        # value flow
         x = self.value_conv(input)
         output_value_conv = tf.keras.layers.Reshape((-1, tf.keras.backend.int_shape(x)[-1]))(x)
         # matrix multiplication for query and key
@@ -45,12 +64,17 @@ class Self_attention_pixel_wise(tf.keras.layers.Layer):
         output_value = tf.keras.layers.Reshape(( height, width,
                                                 tf.keras.backend.int_shape(output_value)[-1]))(output_value)
         output_value=self.output_conv(output_value)
-        # add input to the output value (shortcut conenction)
+        # add input to the output value (shortcut connection)
         output = tf.keras.layers.Add()([output_value, input])
         return output
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0], input_shape[1], input_shape[2],input_shape[3]//self.downsize_factor)
+
+
+#class Multi_head_self_attention_pixel_wise(tf.keras.layers.Layer):
+
+
 
 
 if __name__=="__main__":
