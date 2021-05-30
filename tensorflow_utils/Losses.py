7d6923d4ab14d1304acbb1da1299bc5d21220f18
Denis <ecodenis@yandex.ru>
2021-05-30 15:03:15 +0200

    Focal loss was added.

--- tensorflow_utils/Losses.py
+++ tensorflow_utils/Losses.py
@@ -35,3 +35,70 @@ def weighted_categorical_crossentropy(weights:np.ndarray):
         return loss
 
     return loss
+
+
+
+def categorical_focal_loss(alpha, gamma=2.):
+    """
+    Softmax version of focal loss.
+    author: https://github.com/umbertogriffo
+    When there is a skew between different categories/labels in your data set, you can try to apply this function as a
+    loss.
+           m
+      FL = ∑  -alpha * (1 - p_o,c)^gamma * y_o,c * log(p_o,c)
+          c=1
+      where m = number of classes, c = class and o = observation
+    Parameters:
+      alpha -- the same as weighing factor in balanced cross entropy. Alpha is used to specify the weight of different
+      categories/labels, the size of the array needs to be consistent with the number of classes.
+      gamma -- focusing parameter for modulating factor (1-p)
+    Default value:
+      gamma -- 2.0 as mentioned in the paper
+      alpha -- 0.25 as mentioned in the paper
+    References:
+        Official paper: https://arxiv.org/pdf/1708.02002.pdf
+        https://www.tensorflow.org/api_docs/python/tf/keras/backend/categorical_crossentropy
+    Usage:
+     model.compile(loss=[categorical_focal_loss(alpha=[[.25, .25, .25]], gamma=2)], metrics=["accuracy"], optimizer=adam)
+    """
+
+    alpha = np.array(alpha, dtype=np.float32)
+
+    def categorical_focal_loss_fixed(y_true, y_pred):
+        """
+        :param y_true: A tensor of the same shape as `y_pred`
+        :param y_pred: A tensor resulting from a softmax
+        :return: Output tensor.
+        """
+
+        # Clip the prediction value to prevent NaN's and Inf's
+        epsilon = K.epsilon()
+        y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
+
+        # Calculate Cross Entropy
+        cross_entropy = -y_true * K.log(y_pred)
+
+        # Calculate Focal Loss
+        loss = alpha * K.pow(1 - y_pred, gamma) * cross_entropy
+
+        # Compute mean loss in mini_batch
+        return K.mean(K.sum(loss, axis=-1))
+
+    return categorical_focal_loss_fixed
+
+
+if __name__=="__main__":
+    focal_loss=categorical_focal_loss([0.33, 0.33, 0.33], gamma=2)
+    y_true=np.array([[0,0,1],
+                     [1,0,0],
+                     [1,0,0],
+                     [0,1,0],
+                     [0,0,1]
+                     ])
+    predictions=np.array([[0,0.35,0.65],
+                     [0.3,0.6,0.1],
+                     [0.7,0.3,0],
+                     [0.1,0.54,0.36],
+                     [0,0,1]
+                     ])
+    print(focal_loss(y_true, predictions))
