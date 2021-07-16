79231a0b990a811d68663353ec9cc9c5139deae0
Denis <ecodenis@yandex.ru>
2021-07-16 19:39:04 +0200

    Comments to file were added.

--- feature_extraction/embeddings_extraction.py
+++ feature_extraction/embeddings_extraction.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Utils for extracting deep embeddings from provided data by neural networks (tensorflow)
+
+Module contains functions for deep embeddings extraction by provided extractor, which is tensorflow model.
+
+
+List of functions:
+
+    * extract_deep_embeddings_from_batch_of_images - extracts deep embeddings from provided batch of images (in np.ndarray form)
+    * extract_deep_embeddings_from_images_in_dir - extracts deep embeddings from all images located in provided directory
 """
-TODO: add description
-"""
+__author__ = "Denis Dresvyanskiy"
+__copyright__ = "Copyright 2021"
+__credits__ = ["Denis Dresvyanskiy"]
+__maintainer__ = "Denis Dresvyanskiy"
+__email__ = "denis.dresvyanskiy@uni-ulm.de"
+
+
 import gc
 import os
 from typing import Optional, Tuple, Callable
-
 import tensorflow as tf
 import numpy as np
 import pandas as pd
@@ -14,7 +27,19 @@ from preprocessing.data_preprocessing.image_preprocessing_utils import load_batc
 
 
 def extract_deep_embeddings_from_batch_of_images(images:np.ndarray, extractor:tf.keras.Model, batch_size:Optional[int]=None)->np.ndarray:
-    # TODO: write description
+    """Extracts deep embeddings from provided batch of images via using extractor
+
+    :param images: np.ndarray
+            batch of images in np.ndarray form
+    :param extractor: tf.keras.Model
+            neural network model, implemented in tensorflow
+    :param batch_size: Optional[int]
+            batch size for deep embeddings extraction (in case if overall number of images is high and you want
+            to split calculation into several mini-batches). It is needed, when the whole batch of images cannot be fit into
+            model for one step.
+    :return: np.ndarray
+            extracted embeddings with shape [images.shape[0], num_embeddings]
+    """
     if batch_size is None:
         batch_size=images.shape[0]
     embeddings=extractor.predict(images, batch_size=batch_size)
@@ -23,9 +48,25 @@ def extract_deep_embeddings_from_batch_of_images(images:np.ndarray, extractor:tf
 def extract_deep_embeddings_from_images_in_dir(path_to_dir:str, extractor:tf.keras.Model,
                                                return_type:str='df', batch_size:int=16,
                                                preprocessing_functions:Tuple[Callable[[np.ndarray], np.ndarray], ...]=None)->pd.DataFrame:
-    # TODO: write description
-    if return_type not in ('df','dict'):
-        raise AttributeError('return type can be only \'dict\' or \'df\'. Got %s.'%return_type)
+    """Extracts deep embeddings from all images in provided directory. Returns pd.DataFrame with full paths to each image and
+       deep embeddings.
+
+    :param path_to_dir: str
+            path to directory with images
+    :param extractor: tf.keras.Model
+            neural network model, implemented in tensorflow
+    :param return_type: str
+            can be only df. Other types are not supported yet.
+    :param batch_size: int
+            Parameter for model, indicated how much images per step it should process.
+    :param preprocessing_functions: Tuple[Callable[[np.ndarray], np.ndarray], ...]
+            Tuple of the functions for preprocessing data before extraction deep embeddings. It is needed in case if
+            data should be preprocessed before fit into model.
+    :return: pd.DataFrame
+            DataFrame with absolute paths and deep embeddings for every image
+    """
+    if return_type not in ('df'):
+        raise AttributeError('return type can be only \'df\'. Got %s.'%return_type)
     # load all filenames
     filenames=np.array(os.listdir(path_to_dir))
     # define columns for df
@@ -51,8 +92,3 @@ def extract_deep_embeddings_from_images_in_dir(path_to_dir:str, extractor:tf.ker
         del loaded_images
         gc.collect()
     return embeddings
-
-
-
-if __name__=='__main__':
-    pass
