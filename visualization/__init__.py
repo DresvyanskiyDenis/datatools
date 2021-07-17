2b162265a448bc0e057c4bb0fbe6a78dce5bd317
Denis <ecodenis@yandex.ru>
2021-07-17 23:54:18 +0200

    Comments were added.

--- visualization/__init__.py
+++ visualization/__init__.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" This package contains modules for performing visualisation.
+
+List of modules:
+
+    * ComGradVisualisation - CamGrad visualisation for tensorflow models.
+    * ConfusionMatrixVisualization - Confusion matrices visualisation using predictions and ground truth labels.
+
 """
-TODO: write description of package
-"""
+__author__ = "Denis Dresvyanskiy"
+__copyright__ = "Copyright 2021"
+__credits__ = ["Denis Dresvyanskiy"]
+__maintainer__ = "Denis Dresvyanskiy"
+__email__ = "denis.dresvyanskiy@uni-ulm.de"
+
