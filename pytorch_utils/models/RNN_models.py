2497916f032213d4490ff499e50886a7f06dbe35
Dresvyanskiy <denis.dresvyanskiy@uni-ulm.de>
2022-07-01 18:42:38 +0200

    Code for the dense model was moved to the Dense_models.py files.

--- pytorch_utils/models/RNN_models.py
+++ pytorch_utils/models/RNN_models.py
@@ -1,69 +1,34 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
-__author__ = "Denis Dresvyanskiy"
-__copyright__ = "Copyright 2021"
-__credits__ = ["Denis Dresvyanskiy"]
-__maintainer__ = "Denis Dresvyanskiy"
-__email__ = "denis.dresvyanskiy@uni-ulm.de"
-
-from typing import Tuple, Optional, Union
-
 import torch
 
+class TinyModel(torch.nn.Module):
 
-class DenseModel(torch.nn.Module):
-    activation_functions_mapping = {'relu': torch.nn.ReLU,
-                                    'sigmoid': torch.nn.Sigmoid,
-                                    'tanh': torch.nn.Tanh,
-                                    'softmax': torch.nn.Softmax,
-                                    'linear': torch.nn.Linear
-                                    }
+    def __init__(self):
+        super(TinyModel, self).__init__()
 
-    def __init__(self, input_shape:int, dense_neurons: Tuple[int,...], activations:Union[str,Tuple[str,...]]='relu',
-                    dropout: Optional[float] = 0.3,
-                    regularization:Optional[bool]=None,
-                    output_neurons: Union[Tuple[int,...], int] = 7,
-                    activation_function_for_output:str='softmax'):
-        super(DenseModel, self).__init__()
-        self.input_shape = input_shape
-        self.dense_neurons = dense_neurons
-        self.activations = activations
-        self.dropout = dropout
-        self.regularization = regularization
-        self.output_neurons = output_neurons
-        self.activation_function_for_output = activation_function_for_output
-        # build the model
-        self._build_model()
+        self.linear1 = torch.nn.Linear(100, 200)
+        self.activation = torch.nn.ReLU()
+        self.linear2 = torch.nn.Linear(200, 10)
+        self.softmax = torch.nn.Softmax()
 
     def forward(self, x):
-        for layer in self.layers:
-            x = layer(x)
+        x = self.linear1(x)
+        x = self.activation(x)
+        x = self.linear2(x)
+        x = self.softmax(x)
         return x
 
+tinymodel = TinyModel()
 
-    def _build_model(self):
-        self.layers=[]
-        # input layer
-        self.layers.append(torch.nn.Linear(self.input_shape, self.dense_neurons[0]))
-        self.layers.append(self.activation_functions_mapping[self.activations[0]]())
-        # hidden layers
-        for i in range(len(self.dense_neurons)-1):
-            self.layers.append(torch.nn.Linear(self.dense_neurons[i], self.dense_neurons[i+1]))
-            self.layers.append(self.activation_functions_mapping[self.activations[i+1]]())
-        # output layer
-        self.layers.append(torch.nn.Linear(self.dense_neurons[-1], self.output_neurons))
-        self.layers.append(self.activation_functions_mapping[self.activation_function_for_output]())
+print('The model:')
+print(tinymodel)
 
+print('\n\nJust one layer:')
+print(tinymodel.linear2)
 
+print('\n\nModel params:')
+for param in tinymodel.parameters():
+    print(param)
 
-if __name__ == '__main__':
-    model = DenseModel(input_shape=100, dense_neurons=(200, 300, 500), activations=('relu', 'sigmoid', 'tanh'),
-                    dropout= 0.3,
-                    regularization=None,
-                    output_neurons= 4,
-                    activation_function_for_output='softmax')
-
-    print('model structure:')
-    print(model)
+print('\n\nLayer params:')
+for param in tinymodel.linear2.parameters():
+    print(param)
