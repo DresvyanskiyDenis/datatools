b86dd4f3f2f96478fb0d33aa5c3812fd5fcbbc3c
Denis <ecodenis@yandex.ru>
2022-07-10 15:22:57 +0200

    New script for common decorators has been added. Now it contains only one decorator, which measures the time spent for function execution.

--- decorators/common_decorators.py
+++ decorators/common_decorators.py
@@ -1,3 +1,15 @@
+import functools
+import time
 
-
-# calculating time
+# time calculation of the function
+def timer(func):
+    """Print the runtime of the decorated function"""
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()    # 1
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()      # 2
+        run_time = end_time - start_time    # 3
+        print(f"Function {func.__name__!r} has been executed in {run_time:.4f} secs")
+        return value
+    return wrapper_timer
