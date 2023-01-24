53cc13b951b15b73b0a21f8280eab60d7e30251a
Denis <ecodenis@yandex.ru>
2023-01-24 16:47:31 +0100

    Short description about the model and authors has been added.

--- feature_extraction/retinaface/detector.py
+++ feature_extraction/retinaface/detector.py
@@ -1,3 +1,10 @@
+"""
+The retinaface model is taken from GutHub repositories https://github.com/adas-eye/retinaface
+and https://github.com/biubug6/Pytorch_Retinaface (main repository)
+Thanks to the authors for their work. For more details, please refer to the original repository.
+Do not forget to cite https://github.com/biubug6/Pytorch_Retinaface if you use RetinaFace in your work.
+"""
+
 from __future__ import print_function
 
 import numpy as np
